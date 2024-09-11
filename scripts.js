7c6e8944ea79c0b3f4dee4996a2b7804b5559700
Luan Silva <luansilvae27@gmail.com>
2024-09-11 16:18:08 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.faq-item h3').forEach(item => {
+  item.addEventListener('click', () => {
+    const content = item.nextElementSibling;
+    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  });
+});
